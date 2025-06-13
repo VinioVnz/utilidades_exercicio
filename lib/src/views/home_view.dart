67bc87274e8ca483936e99d50190c8670ca15f06
Vinícius <vinicius.bornhofen47@gmail.com>
2025-06-13 18:38:50 -0300

    atualizei a tela home

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:utilidades/src/controller/temperatura_controller.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final controller = TemperaturaController();
+
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Temperatura Atual em Cogulândia"),
+            ElevatedButton(onPressed: (){}, child: Text("Ver temperatura")),
+          ],
+        ),
+      ),
+    );
   }
 } 
