94f799d1f82ae03b4078dfbe6351f6a588dabb0b
Vinícius <vinicius.bornhofen47@gmail.com>
2025-06-13 19:51:17 -0300

    parte 1 feita

--- lib/src/controller/temperatura_controller.dart
+++ lib/src/controller/temperatura_controller.dart
@@ -1,3 +1,10 @@
+import 'dart:math';
+
 class TemperaturaController {
-  
+  Future<double> carregarTemperaturaInicial() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 25 + Random().nextDouble() * 5,
+    );
+  }
 }
--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,24 +1,66 @@
+  import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:utilidades/src/controller/temperatura_controller.dart';
+  import 'package:utilidades/src/controller/temperatura_controller.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  class HomeView extends StatefulWidget {
+    
+    const HomeView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final controller = TemperaturaController();
-
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Temperatura Atual em Cogulândia"),
-            ElevatedButton(onPressed: (){}, child: Text("Ver temperatura")),
-          ],
-        ),
-      ),
-    );
+    @override
+    State<HomeView> createState() => _HomeViewState();
+    
   }
-} 
+
+  class _HomeViewState extends State<HomeView> {
+    bool carregando = false;
+    double? temperatura;
+    final controller = TemperaturaController();
+ /*    final _streamController = StreamController();
+
+    Stream<double> iniciarAtualizacaoTemperatura(){
+      Timer.periodic(Duration(seconds: 2), (_) {
+        double novaTemp = ...;
+        _streamController.add(novaTemp);
+      }
+      );
+      return _streamController.stream;
+    } */
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if(carregando)
+                CircularProgressIndicator()
+              else if (temperatura != null)
+                Text(
+                  'Temperatura: ${temperatura!.toStringAsFixed(1)} C',
+                  style: TextStyle(fontSize: 32),
+                )
+                else 
+                  Text(
+                    'Clique no botão para ver a temperatura',
+                    style: TextStyle(fontSize: 18),
+                  ),              
+              ElevatedButton(onPressed: () async {
+                setState(() {
+                  carregando = true;
+                  
+                });
+                double temp = await controller.carregarTemperaturaInicial();
+                
+                setState(() {
+                  carregando = false;
+                  temperatura = temp;
+                });
+              }, child: Text("Ver temperatura")),
+            ],
+          ),
+        ),
+      );
+    }
+  } 
